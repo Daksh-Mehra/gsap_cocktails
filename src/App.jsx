b9fcd86cba53aece37ae22e0de4ac3e65db94304
Daksh-Mehra <daksh.m2003bv@gmail.com>
2025-06-25 17:39:47 +0530

    implemented navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {ScrollTrigger,SplitText} from 'gsap/all'
 import gsap from 'gsap';
+import Hero from './components/Hero';
+
+import Navbar from './components/Navbar';
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-3xl'>hello</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <div className='h-dvh bg-black'></div>
+    </main>
   )
 }
 
